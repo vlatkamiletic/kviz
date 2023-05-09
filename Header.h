69c3afd7ab509198f2a9cd764ca14fce7552f524
vlatkamiletic <133046036+vlatkamiletic@users.noreply.github.com>
2023-05-09 20:26:31 +0200

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct pitanje {
+	char pitanje[100];
+	char odgovor1[50];
+	char odgovor2[50];
+	char odgovor3[50];
+	char tocan;
+}PITANJE;
+
+#endif HEADER_H
