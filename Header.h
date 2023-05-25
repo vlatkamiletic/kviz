cbb53a0cac0372c48115306a3a439b65e6b892be
vlatkamiletic <133046036+vlatkamiletic@users.noreply.github.com>
2023-05-25 12:57:40 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,12 +1,30 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+typedef struct igrac
+{
+	char ime[20];
+	int br;
+}IGRAC;
+IGRAC igrac;
+void izbornik(void);
+void unos(void);
+void izlaz(void);
+void ponoviIgru(int);
+void upis_score(int);
+void tablica_rez(void);
+void sort(IGRAC*, IGRAC*);
+void sortiranje_imena(IGRAC*, const int);
+void sortiranje_rezultata(IGRAC*, const int);
+void ispis_imena(IGRAC*, const int);
+void ispis_rezultata(IGRAC*, const int);
+void povratak(void);
+void broj_igraca(void);
+void pretrazivanje_igraca(void);
+void ponovno_pretrazivanje(void);
+void informacije(void);
 
-typedef struct pitanje {
-	char pitanje[100];
-	char odgovor1[50];
-	char odgovor2[50];
-	char odgovor3[50];
-	char tocan;
-}PITANJE;
-
-#endif HEADER_H
+#endif //Header.h
