6d28c858ee8b369d6bf33c86916aa01378d77126
vlatkamiletic <133046036+vlatkamiletic@users.noreply.github.com>
2023-06-06 17:38:27 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -24,12 +24,12 @@ IGRAC igrac;
 void izbornik(void); //8.
 void unos(void);
 void izlaz(void);
-void ponoviIgru(int);
+//void ponoviIgru(int);
 void upis_score(int);
 void brisanje_datoteke();
 void tablica_rez(void);
 void sort(IGRAC*, IGRAC*);
-void sortiranje_imena(IGRAC*, const int);
+void sortiranje_imena(IGRAC*, const int);//11.
 void sortiranje_rezultata(IGRAC*, const int);
 void ispis_imena(IGRAC*, const int);
 void ispis_rezultata(IGRAC*, const int);
