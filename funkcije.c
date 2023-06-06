930c5bbc7ceab81a2a5bc0ee13edc81c407738a7
vlatkamiletic <133046036+vlatkamiletic@users.noreply.github.com>
2023-06-06 17:47:49 +0200

    Update funkcije.c

--- funkcije.c
+++ funkcije.c
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,7 +34,7 @@ void izbornik(void) { //10.
 		case 5:
 			system("cls");
 			izlaz();
-		default: //11.
+		default:
 			system("cls");
 			while ((getchar()) != '\n');
 			printf("Opcija nije dozvoljena, unesite ponovo\n");
@@ -44,14 +44,19 @@ void izbornik(void) { //10.
 
 
 
-
 void unos() {
 	int bodovi = 0;
-	Pitanje pitanja[15];//12.
+
+	Pitanje* pitanja = (Pitanje*)malloc(15 * sizeof(Pitanje)); //13. //14.
+	if (pitanja == NULL) { //15.
+		perror("Error");
+		return;
+	}
 
 	FILE* file = fopen("Pitanja.txt", "r");
 	if (file == NULL) {
-		perror("Greska pri otvaranju datoteke!");//19.
+		perror("Greska pri otvaranju datoteke!");
+		free(pitanja); 
 		return;
 	}
 
@@ -79,7 +84,7 @@ void unos() {
 			scanf(" %s%*c", odgovor);
 
 			if (strlen(odgovor) != 1) {
-				printf("Neispravan unos. Molim vas, unesite samo jedan znak odgovora (A, B, C ili D).\n");
+				printf("Neispravan unos. Molim vas, unesite samo jedan znakova (A, B, C ili D).\n");
 				continue;
 			}
 
@@ -101,14 +106,15 @@ void unos() {
 		else {
 			printf("Netocan odgovor! Tocan odgovor je: %c\n", pitanja[i].tocanOdgovor);
 		}
-		printf("\n\Trenutni broj bodova: %d\n", bodovi);
+		printf("Trenutni broj bodova: %d\n", bodovi);
 
 	}
 
 	printf("\n\nKonacni broj bodova: %d\n", bodovi);
 	upis_score(bodovi);
-}
 
+	free(pitanja);//15.
+}
 
 
 
@@ -119,7 +125,7 @@ void izlaz(void) // funkcija za izlaz iz programa
 	char opcija[3] = { 0 };
 	printf("Jeste li sigurni da zelite izaci iz kviza?\nda\nne\n");
 	scanf("%s", &opcija);
-	if (_strcmpi(da, opcija) == 0)                          
+	if (_strcmpi(da, opcija) == 0)
 	{
 		exit(1);
 	}
@@ -164,19 +170,19 @@ void upis_score(int broj) { //upisuje rezultat u tablicu
 		return;
 	}
 	else {
-	fread(&m, sizeof(int), 1, fp); //17.
-	m++;
-	fseek(fp, 0, SEEK_SET); //17.
-	fwrite(&m, sizeof(int), 1, fp); //17.
-	fseek(fp, 0, SEEK_END);
-	printf("Osvojen iznos: %d\n", broj);
-	printf("Ime igraca: ");
-	scanf(" %[^\n]%*c", igrac->ime);
-	igrac->br = broj;
-	fwrite(igrac, sizeof(IGRAC), 1, fp);
-	system("cls");
-		}
-		fclose(fp);
+		fread(&m, sizeof(int), 1, fp); //17.
+		m++;
+		fseek(fp, 0, SEEK_SET); //17.
+		fwrite(&m, sizeof(int), 1, fp); //17.
+		fseek(fp, 0, SEEK_END);
+		printf("Osvojen iznos: %d\n", broj);
+		printf("Ime igraca: ");
+		scanf(" %[^\n]%*c", igrac->ime);
+		igrac->br = broj;
+		fwrite(igrac, sizeof(IGRAC), 1, fp);
+		system("cls");
+	}
+	fclose(fp);
 }
 
 
@@ -185,10 +191,10 @@ void brisanje_datoteke() {
 
 	printf("Jeste li sigurni da zelite izbrisati tablicu?\n");
 	printf("'da' / 'ne'\n");
-	scanf("%s", odgovor);
+	scanf("%2s", odgovor); 
 
 	if (_strcmpi(odgovor, "da") == 0) {
-		if (remove("tablica.bin") == 0) {//18.
+		if (remove("tablica.bin") == 0) {
 			printf("Tablica je uspjesno izbrisana.\n");
 		}
 		else {
@@ -217,8 +223,8 @@ void tablica_rez(void) { //tablica rezultata koja sortira i pretrazuje //8 podiz
 	{
 		printf("Nema prijasnjih rezultata");
 	}
-	else{
-		fread(&m, sizeof(int), 1, fp); 
+	else {
+		fread(&m, sizeof(int), 1, fp);
 		IGRAC* igrac = NULL;
 		igrac = (IGRAC*)calloc(m, sizeof(IGRAC)); //13. 14. 
 		if (igrac == NULL) { //15.
@@ -230,7 +236,7 @@ void tablica_rez(void) { //tablica rezultata koja sortira i pretrazuje //8 podiz
 		do {
 
 			while ((getchar()) != '\n');
-			broj_igraca(); 
+			broj_igraca();
 			printf("Tablica rezultata:\n1.Rezultati\n2.Ljestvica po bodovima\n3.Pretraga igraca po imenu\n4.Povratak na izbornik\n\n");
 			scanf("%d", &n);
 			switch (n) {
@@ -248,19 +254,21 @@ void tablica_rez(void) { //tablica rezultata koja sortira i pretrazuje //8 podiz
 				povratak();
 				break;
 			case 3:
-				system("cls"); 
+				system("cls");
 				pretrazivanje_igraca();
 				break;
 			case 4:
 				system("cls");
 				izbornik();
-			default://11.
+				break;
+			default:
 				system("cls");
 				printf("Unesite jednu od danih opcija\n\n");
 			}
 		} while (1);
 		free(igrac); //15
 	}
+	fclose(fp);
 }
 
 
@@ -285,7 +293,7 @@ void sortiranje_rezultata(IGRAC* igrac, const int n) {//20.
 }
 
 
-void sortiranje_imena(IGRAC* igrac, const int n) {//20.
+void sortiranje_imena(IGRAC* igrac, const int n) {//20. abc
 	if (igrac == NULL) {
 		perror("Error");
 		exit(1);
@@ -411,7 +419,7 @@ void pretrazivanje_igraca(void) { //21.
 			brojac++;
 		}
 	}
-	if (brojac == 0)                                                                     
+	if (brojac == 0)
 	{
 		printf("Ime nije pronadeno");
 	}
